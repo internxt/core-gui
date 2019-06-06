68e63f1e3d278c25f51544506f6c1560f4042f27
J. Alberto González <JAlbertoGonzalez-github@no-reply.com>
2019-06-06 17:28:08 +0200

    Gulp update

--- tasks/build.js
+++ tasks/build.js
@@ -11,41 +11,44 @@ var paths = {
   copyFromAppDir: ["./**", "!app/views/**/*.vue", "!*.scss"]
 };
 
-gulp.task("clean", function() {
-  return destDir.dir(".", { empty: true });
+gulp.task('clean', function() {
+  return destDir.dirAsync(".", { empty: true });
 });
 
 function copyTask() {
-  return projectDir.copy("app", destDir.path(), {
+  return projectDir.copyAsync("app", destDir.path(), {
     overwrite: true,
     matching: paths.copyFromAppDir
   });
 }
 
-gulp.task('sass', ["clean"], function () {
-  return gulp.src('app/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest(destDir.path()));
-});
+gulp.task('sass', gulp.series('clean', () => gulp.src('app/**/*.scss')
+.pipe(sass.sync().on('error', sass.logError))
+.pipe(gulp.dest(destDir.path()))));
 
 gulp.task('sass:watch', function () {
   gulp.watch('./css/**/*.scss', ['sass']);
 });
 
-gulp.task('vueify', ["copy"], function () {
-  return gulp
-    .src("app/views/**/*.vue")
+gulp.task('copy', copyTask);
+gulp.task('copy-watch', copyTask);
+
+gulp.task('vueify',
+  gulp.series('copy',
+
+ function () { return gulp.src("app/views/**/*.vue")
     .pipe(vueify())
     .on('error', console.error.bind(console))
-    .pipe(gulp.dest(destDir.path() + '/views/'));
-});
+    .pipe(gulp.dest(destDir.path() + '/views/'))
+}
 
-gulp.task("copy", copyTask);
-gulp.task("copy-watch", copyTask);
+    )
+    );
+
 
 gulp.task("watch", function() {
   gulp.watch(paths.copyFromAppDir, { cwd: "app" }, ["copy-watch"]);
   gulp.watch('app/**/*.scss', ["sass:watch"]);
 });
 
-gulp.task("build", ["sass", "vueify"]);
+gulp.task("build", gulp.series('sass', 'vueify'));
