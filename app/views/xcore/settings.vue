4a8932bbc5821cebc3ea92b3336ed34148510516
J. Alberto González <45895462+JAlbertoGonzalez@users.noreply.github.com>
2019-08-12 18:56:30 +0200

    Check reachability on active node

--- app/views/xcore/settings.vue
+++ app/views/xcore/settings.vue
@@ -17,31 +17,49 @@
     <div class="db-widget-container">
       <div class="db-widget__double">
         <div class="db-widget">
-          <div class="db-title">Status</div>
+          <div class="db-title tooltip">
+            Status
+            <span class="tooltiptext">
+              <p>State: {{shareList.shares[0].state}}</p>
+              <p>Is valid: {{shareList.shares[0].isValid}}</p>
+              <p>Is errored: {{shareList.shares[0].isErrored}}</p>
+              <p>Is running: {{shareList.shares[0].isRunning}}</p>
+              <p>Is stopped: {{shareList.shares[0].isStopped}}</p>
+              <p>Connection status: {{shareList.shares[0].meta.farmerState.bridgesConnectionStatus}}</p>
+              <p>Peers: {{shareList.shares[0].meta.farmerState.totalPeers}}</p>
+              <p>Delta: {{shareList.shares[0].meta.farmerState.ntpStatus.delta}}</p>
+            </span>
+          </div>
           <!-- need to wait in order to be displayed properly -->
+          <div class="db-data" v-if="!isReachable">Unreachable</div>
           <div
             class="db-data"
-            v-if="!shareList.shares[0].isValid || (shareList.shares[0].isValid && !shareList.shares[0].isRunning) || (!shareList.shares[0].meta.farmerState.bridgesConnectionStatus && shareList.shares[0].meta.farmerState.bridgesConnectionStatus !== 0)"
+            v-else-if="!shareList.shares[0] || !shareList.shares[0].isValid || (shareList.shares[0].isValid && !shareList.shares[0].isRunning) || (!shareList.shares[0].meta.farmerState.bridgesConnectionStatus && shareList.shares[0].meta.farmerState.bridgesConnectionStatus !== 0)"
+          >Offline</div>
+          <div
+            class="db-data"
+            v-else-if="!shareList.shares[0].isValid || (shareList.shares[0].isValid && !shareList.shares[0].isRunning) || shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 0"
           >Disconnected</div>
           <div
             class="db-data"
-            v-if="!shareList.shares[0].isValid || (shareList.shares[0].isValid && !shareList.shares[0].isRunning) || shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 0"
-          >Disconnected</div>
-          <div
-            class="db-data"
-            v-if="shareList.shares[0].isValid && shareList.shares[0].isRunning && shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 1"
+            v-else-if="shareList.shares[0].isValid && shareList.shares[0].isRunning && shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 1"
           >Connecting...</div>
           <div
             class="db-data"
-            v-if="shareList.shares[0].isValid && shareList.shares[0].isRunning && shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 2"
+            v-else-if="shareList.shares[0].isValid && shareList.shares[0].isRunning && shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 2"
           >Confirming</div>
           <div
             class="db-data"
-            v-if="shareList.shares[0].isValid && shareList.shares[0].isRunning && shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 3"
+            v-else-if="shareList.shares[0].isValid && shareList.shares[0].isRunning && shareList.shares[0].meta.farmerState.bridgesConnectionStatus === 3"
           >Connected</div>
         </div>
         <div class="db-widget">
-          <div class="db-title">Uptime</div>
+          <div class="db-title tooltip">
+            Uptime
+            <span class="tooltiptext">
+              <p>Nº of restarts: {{shareList.shares[0].meta.numRestarts}}</p>
+            </span>
+          </div>
           <div class="db-data">{{shareList.shares[0].meta.uptimeReadable}}</div>
         </div>
       </div>
@@ -70,12 +88,30 @@
     <div class="db-widget-container">
       <div class="db-widget__double">
         <div class="db-widget">
-          <div class="db-title">Port</div>
-          <div class="db-data">{{shareList.shares[0].config.rpcPort}}</div>
+          <div class="db-title tooltip">
+            Port
+            <span class="tooltiptext">
+              <p>Connection type: {{shareList.shares[0].meta.farmerState.portStatus.connectionType}}</p>
+              <p>Address: {{shareList.shares[0].config.rpcAddress}}:{{shareList.shares[0].config.rpcPort}}</p>
+              <p>Status: {{shareList.shares[0].meta.farmerState.portStatus.connectionStatus}}</p>
+            </span>
+          </div>
+          <div
+            class="db-data"
+          >{{shareList.shares[0].config.rpcPort}} {{shareList.shares[0].meta.farmerState.portStatus.listenPort !== shareList.shares[0].config.rpcPort ? '!' : ''}}</div>
         </div>
 
         <div class="db-widget">
-          <div class="db-title">Disk Space</div>
+          <div class="db-title tooltip">
+            Disk Space
+            <span class="tooltiptext">
+              <p>Percentage Used: {{shareList.shares[0].meta.farmerState.percentUsed}}</p>
+              <p>Total Used: {{shareList.shares[0].meta.farmerState.spaceUsed}}</p>
+              <p>Contracts received: {{shareList.shares[0].meta.farmerState.contractCount}}</p>
+              <p>Data received: {{shareList.shares[0].meta.farmerState.dataReceivedCount}}</p>
+              <p>Last activity: {{new Date(shareList.shares[0].meta.farmerState.lastActivity).toISOString()}}</p>
+            </span>
+          </div>
           <div
             class="db-data"
           >{{(shareList.shares[0].meta.farmerState.spaceUsed && shareList.shares[0].meta.farmerState.spaceUsed != '...') ? shareList.shares[0].meta.farmerState.spaceUsed : '0'}} of {{shareList.shares[0].config.storageAllocation}}</div>
@@ -92,6 +128,7 @@ const ConfirmationModal = require("../components/confirmationModal/ConfirmationM
 const UpdateModal = require("../components/updateModal/UpdateModal");
 const rimraf = require("rimraf");
 const Updater = require("./../../lib/updater");
+const request = require('request');
 
 module.exports = {
   name: "nodeSettings",
@@ -105,7 +142,9 @@ module.exports = {
       modalShow: false,
       updateShow: false,
       updateUrl: null,
-      updateVersion: '1.0.0'
+      updateVersion: "1.0.0",
+      isReachable: true,
+      lastTimeReachabilityChecked: new Date()
     };
   },
   created: function() {
@@ -115,15 +154,31 @@ module.exports = {
     });
 
     const updateChecker = new Updater();
-    updateChecker.on('new-update', (newUpdate) => {
-        this.updateUrl = newUpdate.url;
-        this.updateVersion = newUpdate.version;
-        this.updateShow = true;
+    updateChecker.on("new-update", newUpdate => {
+      this.updateUrl = newUpdate.url;
+      this.updateVersion = newUpdate.version;
+      this.updateShow = true;
     });
     updateChecker.checkForUpdates();
     setInterval(() => {
-        updateChecker.checkForUpdates();
+      updateChecker.checkForUpdates();
     }, 60 * 60 * 1000);
+
+    setInterval(() => {
+      let lastActivityDate = this.lastTimeReachabilityChecked;
+      let currentDate = new Date();
+
+      let diff = (currentDate.getTime() - lastActivityDate.getTime()) / 1000;
+      if (diff > 4 * 60 * 1000) {
+      this.lastTimeReachabilityChecked = new Date();
+        this.checkReachability().then(() => {
+          this.isReachable = true;
+        }).catch(err => {
+          this.isReachable = false;
+          console.log('Reachability failed:', err);
+        });
+      }
+    }, 5 * 60 * 1000);
   },
   beforeDestroy: function() {
     this.shareList.actions.status(() => {
@@ -138,12 +193,12 @@ module.exports = {
       return this.store.config.storageAllocation <= this.store.storageAvailable;
     },
     confirmUpdate() {
-        window.open(this.updateUrl, 'Download update', 'frame=true');
+      window.open(this.updateUrl, "Download update", "frame=true");
     },
     cancelUpdate() {
-        this.updateShow = false;
-        return this.updateShow;
-    },
+      this.updateShow = false;
+      return this.updateShow;
+    }, 
     confirmDelete() {
       this.modalShow = true;
       return this.modalShow;
@@ -159,6 +214,22 @@ module.exports = {
         this.shareList.actions.destroy(this.shareList.shares[0].id);
         this.$router.replace({ path: "welcome" });
         this.closeModal();
+      });
+    },
+    checkReachability: function() {
+      return new Promise((resolve, reject) => {
+        request.get(`http://${this.shareList.shares[0].config.rpcAddress}:${this.shareList.shares[0].config.rpcPort}`).on('response', response => {
+          if (response.statusCode === 405 && response.statusMessage === 'Method Not Allowed') {
+            this.isReachable = true;
+            resolve();
+          } else {
+            this.isReachable = false;
+            reject(new Error(JSON.stringify({ code: response.statusCode, text: response.statusMessage})));
+          }
+        }).on('error', err => {
+          this.isReachable = false;
+          reject(err);
+        });
       });
     },
     logData: async function() {
@@ -263,5 +334,39 @@ module.exports = {
 
 .db-widget-container button {
   margin: auto;
+}
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: 150px;
+  background-color: #272533;
+  color: #fff;
+  text-align: left;
+  border-radius: 4px;
+  padding: 2px !important;
+
+  font-size: 10px;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+
+  opacity: 0;
+  transition: opacity 3s;
+  transition-delay: 4s;
+
+  p {
+    margin-left: 3px;
+  }
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
 }
 </style>
